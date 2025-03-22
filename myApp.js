c1e7b51bbdcc50f704730b00af73dd5d2ef2be59
Melios Konstantinos <melios@thinkdesquared.com>
2025-03-22 18:45:04 +0000

    logger

--- myApp.js
+++ myApp.js
@@ -6,6 +6,11 @@ app.use('/public', express.static(
     __dirname + '/public'
 ))
 
+app.use((req, res, next) => {
+    console.log(`${req.method} ${req.path} - ${req.ip}`)
+    next()
+})
+
 app.get('/',
     (req, res) => {
         res.sendFile(__dirname + '/views/index.html')
@@ -15,15 +20,11 @@ app.get('/',
 
 const message = 'Hello json'
 
-console.log(process.env.MESSAGE_STYLE)
-
 app.get("/json", (req, res) => {
     res.json({
       message: process.env.MESSAGE_STYLE === 'uppercase' ? message.toUpperCase() : message
     });
   });
-
-
 
 
 
