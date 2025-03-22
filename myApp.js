678b449bce6ee446712ce38320e851e57b5492f3
Melios Konstantinos <melios@thinkdesquared.com>
2025-03-22 18:36:13 +0000

    first commit

--- myApp.js
+++ myApp.js
@@ -1,12 +1,27 @@
 let express = require('express');
 let app = express();
+require('dotenv').config()
 
-console.log("Hello World")
+app.use('/public', express.static(
+    __dirname + '/public'
+))
+
+app.get('/',
+    (req, res) => {
+        res.sendFile(__dirname + '/views/index.html')
+    }
+)
 
 
+const message = 'Hello json'
 
+console.log(process.env.MESSAGE_STYLE)
 
-
+app.get("/json", (req, res) => {
+    res.json({
+      message: process.env.MESSAGE_STYLE === 'uppercase' ? message.toUpperCase() : message
+    });
+  });
 
 
 
